e7b29b154374dd747132e10049846c09d54117e1
pr <rvpspran@gmail.com>
2023-05-22 14:31:00 +0530

    ipUpdate

--- src/main.js
+++ src/main.js
@@ -2,5 +2,5 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 const app=createApp(App)
-app.config.globalProperties.$url = 'http://localhost:3001/'
+app.config.globalProperties.$url = 'http://3.216.4.3:3000/'
 app.use(router).mount('#app')
