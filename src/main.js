5f1b782147386f68d41c9420f988d808cf5abe82
pr <rvpspran@gmail.com>
2023-05-30 18:46:44 +0530

    GlobalProps

--- src/main.js
+++ src/main.js
@@ -1,6 +1,62 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
-const app=createApp(App)
+const app = createApp(App)
 app.config.globalProperties.$url = 'http://3.216.4.3:3000/'
 app.use(router).mount('#app')
+
+app.config.globalProperties.$contractAddress = "0x0d9455F35Fc243dE68a1a4231022E8573a8c913e"
+app.config.globalProperties.$abi = JSON.parse(
+    `[
+{
+    "inputs": [
+        {
+            "internalType": "string",
+            "name": "_message",
+            "type": "string"
+        }
+    ],
+    "stateMutability": "nonpayable",
+    "type": "constructor"
+},
+{
+    "inputs": [],
+    "name": "getMessage",
+    "outputs": [
+        {
+            "internalType": "string",
+            "name": "",
+            "type": "string"
+        }
+    ],
+    "stateMutability": "view",
+    "type": "function"
+},
+{
+    "inputs": [],
+    "name": "message",
+    "outputs": [
+        {
+            "internalType": "string",
+            "name": "",
+            "type": "string"
+        }
+    ],
+    "stateMutability": "view",
+    "type": "function"
+},
+{
+    "inputs": [
+        {
+            "internalType": "string",
+            "name": "_message",
+            "type": "string"
+        }
+    ],
+    "name": "setMessage",
+    "outputs": [],
+    "stateMutability": "nonpayable",
+    "type": "function"
+}
+]`
+)
